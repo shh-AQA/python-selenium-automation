afae3d6a8a091671b33fde361a91a5c08f4070f6
Saudia HH <saudiazhuss@gmail.com>
2025-06-14 16:00:50 -0400

    Completed homework for Lesson 4 - refactored

---
+++ features/steps/cart_page.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+
+from features.steps.header import cart_icon
+
+CART_SUMMARY = (By.XPATH, "//div[./span[contains(text(), 'subtotal')]]")
+PRODUCT_NAME_IN_CART = (By.CSS_SELECTOR, "[data-test='cartItem-title']")
+
+@when( 'Open cart url' )
+def open_target(context):
+    context.driver.get('https://www.target.com/cart')
+
+@then( 'Verify cart has {amount} item(s)')
+def verify_product_in_cart(context, amount):
+    cart_summary = context.driver.find_element(*CART_SUMMARY).text
+
+    assert f'{amount} item' in cart_summary, f"Expected {amount} item(s) but got {cart_summary}"
+
+@then( 'Verify cart has correct product' )
+def verify_product_in_cart(context):
+    product_name_in_cart = context.driver.find_element(*PRODUCT_NAME_IN_CART).text
+    print('Name in cart: ', product_name_in_cart)
+
+    assert 'Wide Brim' in product_name_in_cart, \
+        f'Expected {context.product_name} did not match {product_name_in_cart}'
+
+    # assert context.product_name[:20] == product_name_in_cart[:20], \
+    #     f'Expected {context.product_name[:20]} did not match {product_name_in_cart[:20]}'
